94f7b37c8cffaddba67394af43e6b2f088a86c28
hk <1808512286@qq.com>
2022-03-08 10:07:02 +0800

    learning

---
+++ src/main/java/com/example/demo/juc/SleepUtils.java
@@ -0,0 +1,12 @@
+package com.example.demo.juc;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepUtils {
+    public static final void second(long seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+        }
+    }
+}
